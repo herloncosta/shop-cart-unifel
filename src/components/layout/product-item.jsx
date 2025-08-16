cf2e6623c25aef85f504c4f572e291240ac52875
herloncosta <herlon36@gmail.com>
2025-08-16 19:57:30 -0300

    feat: estilizando produtos

--- src/components/layout/product-item.jsx
+++ src/components/layout/product-item.jsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom"
+import { formatToBRL } from "../../utils"
 
 export const ProductItem = ({ product }) => {
 	return (
-		<Link to={`/product/${product.id}`}>
-			<div>
-				<div>
-					<img src={product.image} alt={product.description} />
+		<div className='p-6 rounded-xl hover:shadow-xl transition-shadow duration-300 cursor-pointer'>
+			<Link to={`/product/${product.id}`}>
+				<div className='flex flex-col gap-2 justify-center'>
+					<div className='w-full h-[300px]'>
+						<img
+							src={product.image}
+							alt={product.description}
+							className='h-full w-full object-contain'
+						/>
+					</div>
+
+					<div className='p-3'>
+						<h3 className='text-lg font-medium truncate'>{product.title}</h3>
+						<p className='text-sm truncate'>{product.description}</p>
+						<p className='text-xl font-bold mt-2'>
+							{formatToBRL(product.price)}
+						</p>
+						<p>Frete grátis</p>
+					</div>
 				</div>
-				<div>
-					<h3>{product.title}</h3>
-					<p>{product.description}</p>
-					<h2>{product.price}</h2>
-				</div>
-			</div>
-		</Link>
+			</Link>
+		</div>
 	)
 }
