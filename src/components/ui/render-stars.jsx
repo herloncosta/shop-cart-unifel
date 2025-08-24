2c05e0f273f626cf9e07e6919e6263bd83f43bfe
herloncosta <herlon36@gmail.com>
2025-08-23 23:29:58 -0300

    refactor: refatorando componente de estrelas

--- src/components/ui/render-stars.jsx
+++ src/components/ui/render-stars.jsx
@@ -1,10 +1,17 @@
-import { Star } from "lucide-react"
+import { Star, StarHalf } from "lucide-react"
 
 export const RenderStars = ({ rating, size, text }) => {
 	const stars = []
+	const fullStars = Math.floor(rating?.rate)
+	const hasHalfStar = rating?.rate % 1 !== 0
+
 	for (let i = 1; i <= 5; i++) {
-		if (i <= rating?.rate) {
+		if (i <= fullStars) {
 			stars.push(<Star key={i} size={size} color='#ffc107' fill='#ffc107' />)
+		} else if (i === fullStars + 1 && hasHalfStar) {
+			stars.push(
+				<StarHalf key={i} size={size} color='#ffc107' fill='#ffc107' />,
+			)
 		} else {
 			stars.push(<Star key={i} size={size} color='#ccc' />)
 		}
@@ -12,7 +19,7 @@ export const RenderStars = ({ rating, size, text }) => {
 
 	return (
 		<div className='flex gap-2 items-center mt-2'>
-			<span className='flex'>{...stars}</span>
+			<span className='flex'>{stars}</span>
 			<span className={text && "text-xs"}>
 				({rating?.count}) {text && "Classificações"}
 			</span>
